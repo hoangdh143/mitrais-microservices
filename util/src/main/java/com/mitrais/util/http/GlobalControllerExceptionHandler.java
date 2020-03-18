d309b7bcb6ff4593295bddb1a922d290a597793e
HoangDH <huuhoang.dao@gmail.com>
2020-03-18 10:07:16 +0700

    Product Composite depedencies fixed

--- util/src/main/java/com/mitrais/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/com/mitrais/util/http/GlobalControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.mitrais.util.http;
 
+import com.mitrais.util.exceptions.InvalidInputException;
+import com.mitrais.util.exceptions.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -8,8 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import se.magnus.util.exceptions.InvalidInputException;
-import se.magnus.util.exceptions.NotFoundException;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
